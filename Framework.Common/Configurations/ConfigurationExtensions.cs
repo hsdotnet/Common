9ed7f4412174256656d6858853ed8ea02a34403a
hsdotnet <630165235@qq.com>
2016-03-18 16:37:37 +0800

    commit

--- Framework.Common/Configurations/ConfigurationExtensions.cs
+++ Framework.Common/Configurations/ConfigurationExtensions.cs
@@ -6,18 +6,34 @@ namespace Framework.Common.Configurations
 {
     public static class ConfigurationExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static Configuration UseAutofac(this Configuration configuration)
         {
             ObjectContainer.SetContainer(new AutofacObjectContainer());
             return configuration;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static Configuration UseJsonNet(this Configuration configuration)
         {
             configuration.SetDefault<IObjectSerializer, JsonObjectSerializer>(new JsonObjectSerializer());
             return configuration;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="configFile"></param>
+        /// <returns></returns>
         public static Configuration UseLog4Net(this Configuration configuration, string configFile = "log4net.config")
         {
             configuration.SetDefault<ILoggerFactory, Log4NetLoggerFactory>(new Log4NetLoggerFactory(configFile));
