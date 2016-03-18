3e41c933f64e447dcf834d1ff2c3b968b71327a0
hsdotnet <630165235@qq.com>
2016-03-18 16:19:27 +0800

    commit

--- Framework.Common/Configurations/ConfigurationExtensions.cs
+++ Framework.Common/Configurations/ConfigurationExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Framework.Common.Serialization;
+﻿using Framework.Common.Serialization;
 using Framework.Common.Ioc;
 using Framework.Common.Logger;
 
