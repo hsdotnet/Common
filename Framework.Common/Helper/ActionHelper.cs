84e67a2bedf41e662fd18db7cfa2183a7db7c90f
hsdotnet <630165235@qq.com>
2016-04-14 09:27:24 +0800

    commit

---
+++ Framework.Common/Helper/ActionHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Framework.Common.Helper
+{
+    public class ActionHelper
+    {
+        public static void EatException(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public static T EatException<T>(Func<T> action, T defaultValue = default(T))
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static TimeSpan StopwatchAction(Action action)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            action();
+
+            sw.Stop();
+
+            return sw.Elapsed;
+        }
+    }
+}
