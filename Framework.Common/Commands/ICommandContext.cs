8293f379d2e837b31a82329743843f959fb55440
hsdotnet <hsdotnet@DESKTOP-5QPMPDV>
2016-04-14 21:28:17 +0800

    commit

--- Framework.Common/Commands/ICommandContext.cs
+++ Framework.Common/Commands/ICommandContext.cs
@@ -6,10 +6,33 @@ namespace Framework.Common.Commands
 {
     public interface ICommandContext : ICommandUnitOfWork, IDisposable
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TAggregateRoot"></typeparam>
+        /// <typeparam name="TPrimaryKey"></typeparam>
+        /// <param name="aggregateRoot"></param>
+        /// <param name="sql"></param>
         void Add<TAggregateRoot, TPrimaryKey>(TAggregateRoot aggregateRoot, string sql) where TAggregateRoot : IEntity<TPrimaryKey>;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TAggregateRoot"></typeparam>
+        /// <typeparam name="TPrimaryKey"></typeparam>
+        /// <param name="aggregateRoot"></param>
+        /// <param name="sql"></param>
+        /// <returns></returns>
         TPrimaryKey AddAndReturnPrimaryKey<TAggregateRoot, TPrimaryKey>(TAggregateRoot aggregateRoot, string sql) where TAggregateRoot : IEntity<TPrimaryKey>;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TAggregateRoot"></typeparam>
+        /// <typeparam name="TPrimaryKey"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="sql"></param>
+        /// <returns></returns>
         TAggregateRoot Get<TAggregateRoot, TPrimaryKey>(TPrimaryKey id, string sql);
     }
 }
