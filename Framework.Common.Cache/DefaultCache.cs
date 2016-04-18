ec8b54110ef3b02092279bfcaea77c38809de335
hsdotnet <630165235@qq.com>
2016-04-18 14:31:09 +0800

    commit

---
+++ Framework.Common.Cache/DefaultCache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Common.Cache
+{
+    public class DefaultCache : ICache
+    {
+        public string RegionName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Exists(string cacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set(string cacheKey, object cacheValue, bool isAbsoluteExpire, int expireMinutes = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string cacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string cacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Framework.Common.Cache/ICache.cs
@@ -0,0 +1,50 @@
+﻿namespace Framework.Common.Cache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICache
+    {
+        /// <summary>
+        /// 缓存区域名称
+        /// </summary>
+        string RegionName { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <returns></returns>
+        bool Exists(string cacheKey);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="cacheValue"></param>
+        /// <param name="isAbsoluteExpire"></param>
+        /// <param name="expireMinutes"></param>
+        /// <returns></returns>
+        bool Set(string cacheKey, object cacheValue, bool isAbsoluteExpire, int expireMinutes = 0);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="cacheKey"></param>
+        /// <returns></returns>
+        T Get<T>(string cacheKey);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <returns></returns>
+        bool Remove(string cacheKey);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void RemoveAll();
+    }
+}
